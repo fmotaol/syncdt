29ea2e2045fc69cf2c4138933a0a8b43052bc978
Fabricio (EMBASA) <fabricio.oliveira@embasa.ba.gov.br>
2026-08-10 11:48:59 -0300

    fetch_pull

--- fetch_pull.bat
+++ fetch_pull.bat
@@ -1,12 +1,27 @@
 @echo off
 setlocal enabledelayedexpansion
+
 echo ==========================================
 echo Iniciando processo de fetch e pull...
 echo ==========================================
 
 git fetch --all
-git pull
 
+echo.
+echo Branches disponiveis:
+git branch -a
+echo.
+
+set /p branch="Digite a branch para pull (enter para main): "
+
+if "%branch%"=="" set branch=main
+
+echo.
+echo Realizando pull da branch %branch%
+
+git pull origin %branch%
+
+echo.
 echo ==========================================
 echo Fetch/Pull concluido!
 pause
